e65f66342e5a5aec556e66af9af79127160f7ca9
Esl4mx <eslamsaied836@gmail.com>
2025-01-05 09:19:21 +0200

    disable device preview

--- lib/main.dart
+++ lib/main.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(DevicePreview(
-      enabled: true, builder: (context) => const ResponsiveDashBoard()));
+      enabled: false, builder: (context) => const ResponsiveDashBoard()));
 }
 
 class ResponsiveDashBoard extends StatelessWidget {
